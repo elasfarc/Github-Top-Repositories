9be17777d041cdd9188e343798ebbea08faa06a0
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-08-11 16:37:48 +0200

    Add RepoGrid

--- app/components/popular.js
+++ app/components/popular.js
@@ -1,6 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { getRepos } from "../util/api";
+import {
+  FaUser,
+  FaStar,
+  FaCodeBranch,
+  FaExclamationTriangle,
+} from "react-icons/fa";
 
 function LangaugesNav(props) {
   const LANGS = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
@@ -28,6 +34,53 @@ LangaugesNav.propTypes = {
   selected: PropTypes.string.isRequired,
   handleClick: PropTypes.func.isRequired,
 };
+
+function ReposGrid({ repos }) {
+  return (
+    <ul className="grid">
+      {repos.map((repo, index) => {
+        const { name, owner, html_url, stargazers_count, forks, open_issues } =
+          repo;
+        const { login, avatar_url } = owner;
+
+        return (
+          <li key={html_url} className="repo bg-light">
+            <h4 className="header-lg center-text">#{index + 1}</h4>
+            <img
+              className="avatar"
+              src={avatar_url}
+              alt={`Avatar for ${login}`}
+            />
+            <h2 className="center-text">
+              <a className="link" href={html_url}>
+                {login}
+              </a>
+            </h2>
+            <ul className="card-list">
+              <li>
+                <FaUser color="rgb(255, 191, 116)" size={22} />
+                <a href={`https://github.com/${login}`}>{login}</a>
+              </li>
+              <li>
+                <FaStar color="rgb(255, 215, 0)" size={22} />
+                {stargazers_count.toLocaleString()} stars
+              </li>
+              <li>
+                <FaCodeBranch color="rgb(129, 195, 245)" size={22} />
+                {forks.toLocaleString()} forks
+              </li>
+              <li>
+                <FaExclamationTriangle color="rgb(241, 138, 147)" size={22} />
+                {open_issues.toLocaleString()} open
+              </li>
+            </ul>
+          </li>
+        );
+      })}
+    </ul>
+    // <pre>{JSON.stringify(repos, null, 2)}</pre>
+  );
+}
 
 export default class Popular extends React.Component {
   constructor(props) {
@@ -78,7 +131,8 @@ export default class Popular extends React.Component {
         {this.isLoading() && <p>LOADING...</p>}
         {this.state.error && <p>{this.state.error}</p>}
         {this.state.repos[lang] && (
-          <pre>{JSON.stringify(this.state.repos[lang], null, 2)}</pre>
+          //<pre>{JSON.stringify(this.state.repos[lang], null, 2)}</pre>
+          <ReposGrid repos={this.state.repos[lang]} />
         )}
       </>
     );
