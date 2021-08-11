629ef203150de5a9084645ffc616451e32d8978d
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-08-11 15:48:40 +0200

    cashing fetched repos inside component's local state

--- app/components/popular.js
+++ app/components/popular.js
@@ -32,31 +32,42 @@ LangaugesNav.propTypes = {
 export default class Popular extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { lang: "All", repos: null, error: null };
+    this.state = { lang: "All", repos: {}, error: null };
     this.handleClick = this.handleClick.bind(this);
   }
 
   async componentDidMount() {
-    this.setState({ error: null, repos: await getRepos(this.state.lang) });
+    const { lang } = this.state;
+    this.setState({
+      error: null,
+      repos: { [lang]: await getRepos(this.state.lang) },
+    });
   }
 
   handleClick(lang) {
     this.setState({
       lang,
       error: null,
-      repos: null,
     });
-    getRepos(lang)
-      .then((repos) => {
-        this.setState({ repos, error: null });
-      })
-      .catch((error) => {
-        this.setState({ error });
-      });
+    if (!this.state.repos[lang]) {
+      getRepos(lang)
+        .then((data) => {
+          // this.setState({ repos, error: null });
+          this.setState(({ repos }) => ({
+            repos: {
+              ...repos,
+              [lang]: data,
+            },
+          }));
+        })
+        .catch((error) => {
+          this.setState({ error });
+        });
+    }
   }
 
   isLoading = () => {
-    return this.state.error === null && this.state.repos === null;
+    return this.state.error === null && !this.state.repos[this.state.lang];
   };
 
   render() {
@@ -64,12 +75,10 @@ export default class Popular extends React.Component {
     return (
       <>
         <LangaugesNav handleClick={this.handleClick} selected={lang} />
-        {/* {console.log("*****", this.state.repos)}
-        {console.log("*****", this.isLoading)} */}
         {this.isLoading() && <p>LOADING...</p>}
         {this.state.error && <p>{this.state.error}</p>}
-        {this.state.repos && (
-          <pre>{JSON.stringify(this.state.repos, null, 2)}</pre>
+        {this.state.repos[lang] && (
+          <pre>{JSON.stringify(this.state.repos[lang], null, 2)}</pre>
         )}
       </>
     );
