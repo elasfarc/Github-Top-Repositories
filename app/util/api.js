1a141c0e548032068f39ae9759a003ddf4693b51
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-08-13 21:24:47 +0200

    Fix ESlint errors

--- app/util/api.js
+++ app/util/api.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/prefer-default-export
 export const getRepos = (lang) => {
   const endpoint = window.encodeURI(
-    `https://api.github.com/search/repositories?q=stars:>1+language:${lang}&sort=stars&order=desc&type=Repositories`
+    `https://api.github.com/search/repositories?q=stars:>1+language:${lang}&sort=stars&order=desc&type=Repositories`,
   );
   return fetch(endpoint)
     .then((res) => res.json())
